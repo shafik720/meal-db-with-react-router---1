20044f361a87ddd5f79d142a539e66910135d92f
shafik720 <shafikrasel5@gmail.com>
2022-10-31 22:47:06 +0600

    heldsfgs

--- src/Componets/SingleMeal/SingleMeal.js
+++ src/Componets/SingleMeal/SingleMeal.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const SingleMeal = () => {
+
+    let params = useParams();
+
     return (
         <div>
-            <h2>Meal Details Coming Soon ....... </h2>
+            <h2>Meal Details : {params.details} </h2>
         </div>
     );
 };
