2bffc0511f14e727a7b997539bcc8b1385b4449d
shafik720 <shafikrasel5@gmail.com>
2022-10-31 12:53:21 +0600

    dfgfdsg

---
+++ src/Componets/Food/Food.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Food.css'
+
+const Food = () => {
+    return (
+        <div>
+            <h2>Hello From Food</h2>
+        </div>
+    );
+};
+
+export default Food;
