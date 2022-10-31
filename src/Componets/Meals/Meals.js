521c6caaf57e83d69316a2d95b5a4f2ee96d3883
shafik720 <shafikrasel5@gmail.com>
2022-10-31 22:19:39 +0600

    heldsfgs

--- src/Componets/Meals/Meals.js
+++ src/Componets/Meals/Meals.js
@@ -3,11 +3,11 @@ import './Meals.css'
 
 const Meals = (props) => {
     let {idMeal, strArea, strCategory, strMeal, strMealThumb} = props.index ;
-    function singleFood(e){
-        console.log(e.target);
+    function singleFood(id){
+        console.log(id);
     }
     return (
-        <div onClick={singleFood} className="meal-card">
+        <div onClick={()=>singleFood(idMeal)} className="meal-card">
             <img src={strMealThumb} alt=""/>
             <h3>Name : {strMeal}</h3>
         </div>
