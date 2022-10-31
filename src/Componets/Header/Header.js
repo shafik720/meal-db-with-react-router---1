509268b504e0f42a2003880d60cc182d6761c849
shafik720 <shafikrasel5@gmail.com>
2022-10-31 13:00:22 +0600

    dfgfdg

--- src/Componets/Header/Header.js
+++ src/Componets/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h2>Hello From Header</h2>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/food">Foods</Link>
         </div>
     );
 };
