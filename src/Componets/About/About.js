3e1161f1e626ca2256a415098a82457cf6fa2e70
shafik720 <shafikrasel5@gmail.com>
2022-10-31 12:54:09 +0600

    dfgfdsg

---
+++ src/Componets/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css'
+const About = () => {
+    return (
+        <div>
+            <h2>Know About Us </h2>
+        </div>
+    );
+};
+
+export default About;
