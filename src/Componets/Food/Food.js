e2f4bba919d6dc712f0734953c1740c23326f997
shafik720 <shafikrasel5@gmail.com>
2022-10-31 14:37:16 +0600

    dsfgsdg

--- src/Componets/Food/Food.js
+++ src/Componets/Food/Food.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 import './Food.css'
 
 const Food = () => {
     return (
         <div>
-            <h2>Hello From Food</h2>
+            <div className="search-div col-lg-6">
+                <Form.Control
+                    placeholder="Search Your Food here . . . . . . ."
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                />
+                <Button variant="warning">Search</Button>
+            </div>
         </div>
     );
 };
