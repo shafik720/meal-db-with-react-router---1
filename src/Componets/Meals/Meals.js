cd66800147ce63fb81d849cd63aa829dea29fc64
shafik720 <shafikrasel5@gmail.com>
2022-10-31 22:21:39 +0600

    heldsfgs

--- src/Componets/Meals/Meals.js
+++ src/Componets/Meals/Meals.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import './Meals.css'
 
 const Meals = (props) => {
     let {idMeal, strArea, strCategory, strMeal, strMealThumb} = props.index ;
+
+    let navigate = useNavigate();
     function singleFood(id){
-        console.log(id);
+        let url = `/food/${id}`;
+        navigate(url);
     }
     return (
         <div onClick={()=>singleFood(idMeal)} className="meal-card">
