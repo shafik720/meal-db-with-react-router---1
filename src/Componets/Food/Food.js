077fc28700668dac61330ff4185ca382996d4258
shafik720 <shafikrasel5@gmail.com>
2022-10-31 18:31:01 +0600

    heldsfgs

--- src/Componets/Food/Food.js
+++ src/Componets/Food/Food.js
@@ -3,15 +3,18 @@ import { Button, Form } from 'react-bootstrap';
 import './Food.css'
 
 const Food = () => {
+    function showFood(e){
+        console.log(e.target.value);
+    }
     return (
         <div>
             <div className="search-div col-lg-6">
                 <Form.Control
                     placeholder="Search Your Food here . . . . . . ."
                     aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
+                    aria-describedby="basic-addon2" onChange={showFood}
                 />
-                <Button variant="warning">Search</Button>
+                <Button variant="warning" className="btn-lg">Search</Button>
             </div>
         </div>
     );
