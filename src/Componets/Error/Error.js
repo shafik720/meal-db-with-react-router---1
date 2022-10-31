a82e0b38333ace292d6705c4bfa93c5ebe2ecedc
shafik720 <shafikrasel5@gmail.com>
2022-10-31 12:57:02 +0600

    dfgfdsg

--- src/Componets/Error/Error.js
+++ src/Componets/Error/Error.js
@@ -4,7 +4,7 @@ import './Error.css'
 const Error = () => {
     return (
         <div>
-            <h1>Error 404 Not Found</h1>
+            <h1 style={{color:"red"}}>Error 404 Not Found</h1>
         </div>
     );
 };
