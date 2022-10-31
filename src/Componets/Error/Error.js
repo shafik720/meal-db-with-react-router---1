c3bc883bf2711cbb188966c93a955ca067844fe7
shafik720 <shafikrasel5@gmail.com>
2022-10-31 12:56:44 +0600

    dfgfdsg

---
+++ src/Componets/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Error.css'
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Error 404 Not Found</h1>
+        </div>
+    );
+};
+
+export default Error;
