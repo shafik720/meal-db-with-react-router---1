46bc0cee1562dff385caa7632d1c4e5e3002a23a
shafik720 <shafikrasel5@gmail.com>
2022-10-31 13:18:50 +0600

    dfgfdg

--- src/Componets/Header/Header.js
+++ src/Componets/Header/Header.js
@@ -4,11 +4,13 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="header-div">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/food">Foods</Link>
+        <div className="">
+            <div className="header-div">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/food">Foods</Link>
+            </div>            
         </div>
     );
 };
